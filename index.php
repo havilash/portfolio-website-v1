e2331eb2aa54c5904119b34df00a2d3b96a0782b
Havilash <92213955+Havilash@users.noreply.github.com>
2022-07-07 15:44:47 +0200

    create navbar

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("./components/meta.html") ?>
+
+    <link rel="stylesheet" href="./css/styles.css">
+
+    <title>Sivaratnam Havilash</title>
+</head>
+<body>
+    <?php include("./components/navbar.html") ?>
+
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+</body>
+</html>
