31db33692c3b81496ff13ee087fce4f9919a92a8
Havilash Sivaratnam <havilash2005@gmail.com>
2022-10-15 10:54:46 +0200

    update

--- havilash_sivaratnam_v2/server/src/index.js
+++ havilash_sivaratnam_v2/server/src/index.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const database = require('./database/database')
+const database = require('./database/database');
 
-const port = 3000
+const port = 3000;
 
-const app = express()
+const app = express();
 const conn = database.conn;
 
 app.get('/users', (req, res) => {
-    conn.query("SELECT * FROM users", function(err, result, fields){
+    conn.query("SELECT * FROM users", function(err, result){
         if (err) throw err;
-        res.send(result)
+        res.send(result);
     })
-})
+});
 
 app.listen(port, () => {
     console.log(`[CONNECTED] server listening to port ${port}`);
-})
+});
