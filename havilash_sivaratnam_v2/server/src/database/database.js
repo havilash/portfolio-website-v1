33a3d8b510c64715d6f0d9b7d0f1d66d653b61f1
Havilash Sivaratnam <havilash2005@gmail.com>
2022-10-28 20:26:55 +0200

    build: test

--- havilash_sivaratnam_v2/server/src/database/database.js
+++ havilash_sivaratnam_v2/server/src/database/database.js
@@ -9,16 +9,64 @@ const mysql = require('mysql');
 //     database: "db_havilash_sivaratnam"
 // });
 
-const conn = mysql.createConnection({
-    host: "172.29.4.61",
-    user: "db_001396",
-    password: "CZXJtTxFcnAZ",
-    database: "inf_293_21g_m293user17"
-});
+// const conn = mysql.createConnection({
+//     host: "172.29.4.61",
+//     user: "db_001396",
+//     password: "CZXJtTxFcnAZ",
+//     database: "inf_293_21g_m293user17"
+// });
 
-conn.connect((err) => {
-    if (err) throw err;
-    console.log("[CONNECTED] Database connected");
-});
+// conn.connect((err) => {
+//     if (err) throw err;
+//     console.log("[CONNECTED] Database connected");
+// });
 
-module.exports.conn = conn;
+
+
+// const mysql = require('mysql2');
+// const { Client } = require('ssh2');
+// const sshClient = new Client();
+
+// const dbServer = {
+//     host: "172.29.4.61",
+//     user: "db_001396",
+//     password: "CZXJtTxFcnAZ",
+//     database: "inf_293_21g_m293user17"
+// }
+// const tunnelConfig = {
+//     host: "inf-293-21g-m293user17.iet-gibb.net",
+//     username: "inf-293-21g-m293user17",
+//     password: "CZXJtTxFcnAZ"
+// }
+// const forwardConfig = {
+//     srcHost: '127.0.0.1',
+//     srcPort: 3306,
+//     dstHost: dbServer.host,
+//     dstPort: 3306
+// };
+// var connection;
+// const SSHConnection = new Promise((resolve, reject) => {
+//     sshClient.on('ready', () => {
+//         sshClient.forwardOut(
+//         forwardConfig.srcHost,
+//         forwardConfig.srcPort,
+//         forwardConfig.dstHost,
+//         forwardConfig.dstPort,
+//         (err, stream) => {
+//             if (err) reject(err);
+//             const updatedDbServer = {
+//                 ...dbServer,
+//                 stream
+//             };
+//             connection =  mysql.createConnection(updatedDbServer);
+//             connection.connect((error) => {
+//             if (error) {
+//                 reject(error);
+//             }
+//             resolve(connection);
+//             });
+//         });
+//     }).connect(tunnelConfig);
+// });
+
+module.exports.conn = "connection";
