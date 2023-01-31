ca2474f5626dc59c22e38198c32ab70e18e7dafd
Havilash Sivaratnam <havilash2005@gmail.com>
2023-01-31 10:39:47 +0100

    update

--- havilash_sivaratnam/server/src/database/database.js
+++ havilash_sivaratnam/server/src/database/database.js
@@ -5,7 +5,7 @@ const dbUrl = "postgres://db_001396_user:01TCuLzgrv66YjkS3ThgVLDl08orAMJS@dpg-cd
 const client = new Client(dbUrl);
 
 client.connect((err) => {
-    if (err) console.log("[NOT CONNECTED] Database not connected");
+    if (err) return console.log("[NOT CONNECTED] Database not connected");
     console.log("[CONNECTED] Database connected");
 });
 
