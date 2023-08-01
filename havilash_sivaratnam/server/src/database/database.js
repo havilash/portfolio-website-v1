e6bbeebf3a1900db64a7a868eba23c22849392b6
Havilash Sivaratnam <havilash2005@gmail.com>
2023-08-01 22:09:24 +0200

    update

--- havilash_sivaratnam/server/src/database/database.js
+++ havilash_sivaratnam/server/src/database/database.js
@@ -1,13 +1,14 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
-const dbUrl = "postgres://db_001396_user:01TCuLzgrv66YjkS3ThgVLDl08orAMJS@dpg-cdgn3qaen0hj5eajnk30-a.frankfurt-postgres.render.com/db_001396?ssl=true"
+// const dbUrl = "postgres://db_001396_user:01TCuLzgrv66YjkS3ThgVLDl08orAMJS@dpg-cdgn3qaen0hj5eajnk30-a.frankfurt-postgres.render.com/db_001396?ssl=true"
+const dbUrl =
+  "postgres://sqhupmmi:onHbnOM5EtJvyTbXCtdFHrisz9suvfau@horton.db.elephantsql.com/sqhupmmi?ssl=true";
 
 const client = new Client(dbUrl);
 
 client.connect((err) => {
-    if (err) return console.log("[NOT CONNECTED] Database not connected");
-    console.log("[CONNECTED] Database connected");
+  if (err) return console.log("[NOT CONNECTED] Database not connected");
+  console.log("[CONNECTED] Database connected");
 });
-
 
 module.exports.conn = client;
